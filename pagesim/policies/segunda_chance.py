a03188f4a1d0d8a3fbeaa3a3bff83edbc1698858
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2025-12-17 23:01:20 -0300

    Update segunda_chance.py
    Implementação do Segunda Chance/Clock

--- pagesim/policies/segunda_chance.py
+++ pagesim/policies/segunda_chance.py
@@ -5,10 +5,36 @@ from pagesim.core.models import Page
 
 class SegundaChancePolicy(ReplacementPolicy):
     def __init__(self, ram: Ram):
-        pass
+        self.ram = ram
+        self.fila: list[int] = [] # Fila de páginas 
+        self.pos: int = 0 # Posição inicial na fila
 
     def on_access(self, page_id: int, page: Page):
-        pass
+        # Quando a página entra ela tem 1 chance a mais de não ser retirada, essa chance renova 
+        # Se a página for refernciada
+        page.reference_bit = True 
+
+        # Se a página já não estiver na fila, ela é inserida
+        if(page_id not in self.fila):
+            self.fila.append(page_id)
 
     def victim(self) -> int:
-        pass
+        while True:
+            page_id = self.fila[self.pos]
+            page = self.ram.pages[page_id]
+
+            # Se a página não tiver mais uma chance...
+            if(not page.reference_bit):
+                # Remove a página da fila...
+                self.fila.pop(self.pos)
+                # e ajusta o ponteiro do relógio, se necessário
+                if(self.pos >= len(self.fila)):
+                    self.pos = 0
+                return page_id
+            else:
+                # Retira a segunda chance...
+                page.reference_bit = False
+                # e reposiciona o ponteiro para a próxima página
+                self.pos = (self.pos + 1) % len(self.fila)
+
+
